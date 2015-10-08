1ee6827aa50a9eb6714eaa4a1cb8a1ac043d4bf8
mpopijac <matija.popijac@hotmail.com>
2015-10-08 11:52:14 +0200

    Initial commit

---
+++ app/src/androidTest/java/in4matics_team/in4maticsquiz/ApplicationTest.java
@@ -0,0 +1,13 @@
+package in4matics_team.in4maticsquiz;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
