a7f71d568ef4360bdfbb72bab7ce13dfdf9bcc29
tjarcov <tea.jarcov@gmail.com>
2015-12-07 11:28:56 +0100

    Rang liste- ispravak

--- app/src/main/java/in4matics_team/in4maticsquiz/RangListaAdapter.java
+++ app/src/main/java/in4matics_team/in4maticsquiz/RangListaAdapter.java
@@ -20,8 +20,9 @@ import in4matics_team_local.db.Rezultat;
  */
 public class RangListaAdapter extends ArrayAdapter<Rezultat> {
     private List<Rezultat> rezultatArrayList;
-    private long brojac=0;
+    private int brojac=0;
     Korisnik korisnickoIme;
+
 
     public RangListaAdapter(Context context, int textViewResourceId, List<Rezultat> objects) {
         super(context, textViewResourceId, objects);
@@ -29,62 +30,63 @@ public class RangListaAdapter extends ArrayAdapter<Rezultat> {
     }
 
 
-        //ViewGroup parent -> viška
+    //ViewGroup parent -> viška
 
     public View getView(int position, View convertView, ViewGroup parent){
 
         // assign the view we are converting to a local variable
         View v = convertView;
+        Integer odabraniRazred = PrijavljeniKorisnik.getInstance().getOdabraniRazred();
+
+
 
         // first check to see if the view is null. if so, we have to inflate it.
         // to inflate it basically means to render, or show, the view.
         if (v == null) {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            v = inflater.inflate(R.layout.rang_liste_fragment, null);
+            v = inflater.inflate(R.layout.rang_liste_fragment, parent, false);
         }
 
-		/*
-		 * Recall that the variable position is sent in as an argument to this method.
-		 * The variable simply refers to the position of the current object in the list. (The ArrayAdapter
-		 * iterates through the list we sent it)
-		 *
-		 * Therefore, i refers to the current Item object.
-		 */
-        Rezultat i = rezultatArrayList.get(position);
+        //   TextView txtuser = (TextView) v.findViewById(R.id.naslov);
 
-        if (i != null) {
+        //     txtuser.setText(Integer.toString(odabraniRazred)+". razred");
 
-            brojac++;
-
-            // This is how you obtain a reference to the TextViews.
-            // These TextViews are created in the XML files we defined.
-
-            TextView rBroj = (TextView) v.findViewById(R.id.rBroj);
-            TextView korIme = (TextView) v.findViewById(R.id.korisniko_ime);
-            TextView bodovi = (TextView) v.findViewById(R.id.brBodova);
+        String[] korisnici= new String[rezultatArrayList.size()];
+        Integer[] brojaci= new Integer[rezultatArrayList.size()];
+        Long[] bodovi2= new Long[rezultatArrayList.size()];
 
 
+        int z=0;
+        int g=1;
 
-            korisnickoIme = new Select().from(Korisnik.class).where("IDkorisnik==?",i.getIDkorisnik()).executeSingle();
+        for (Rezultat rez: rezultatArrayList) {
+
+            Korisnik korisnickoIme;
+            korisnickoIme = new Select().from(Korisnik.class).where("IDkorisnik==?", rez.getIDkorisnik()).executeSingle();
             String username = korisnickoIme.getKorisnickoIme();
-            //rezultatArrayList = new Select().all().from(Rezultat.class).execute();
+
+            korisnici[z]=username;
+            brojaci[z]=g;
+            bodovi2[z]=rez.getBodovi();
 
 
-            // check to see if each individual textview is null.
-            // if not, assign some text!
-            if (rBroj != null){
-                rBroj.setText(Long.toString(brojac));
-            }
-
-
-            if (korIme != null){
-                korIme.setText(username);
-            }
-            if (bodovi != null){
-                bodovi.setText(Long.toString(i.getBodovi()));
-            }
+            z++;
+            g++;
 
         }
+
+        TextView rBroj = (TextView) v.findViewById(R.id.rBroj);
+        TextView korIme = (TextView) v.findViewById(R.id.korisniko_ime);
+        TextView bodovi = (TextView) v.findViewById(R.id.brBodova);
+
+
+        //       System.out.println(Long.toString(brojac)+username+i.getBodovi()+"bla");
+
+        rBroj.setText(Integer.toString(brojaci[position])+". ");
+        korIme.setText(korisnici[position]);
+        bodovi.setText(bodovi2[position].toString());
+
+
 
         // the view must be returned to our activity
         return v;
