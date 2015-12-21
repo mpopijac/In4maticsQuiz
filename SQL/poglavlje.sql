329646d1d8eac99695cb0ed6ab67f10da692f7f9
tjarcov <tea.jarcov@gmail.com>
2015-12-21 23:01:12 +0100

    Dokumentacija, SQL
    Dodani burndown chartovi, upisani dnevni napori
    dodane ažurirane tablice iz baze

--- SQL/poglavlje.sql
+++ SQL/poglavlje.sql
@@ -4,7 +4,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Računalo: localhost
--- Vrijeme generiranja: Pro 02, 2015 u 07:58 PM
+-- Vrijeme generiranja: Pro 21, 2015 u 09:56 PM
 -- Verzija poslužitelja: 10.0.20-MariaDB
 -- PHP verzija: 5.2.17
 
@@ -31,17 +31,20 @@ CREATE TABLE IF NOT EXISTS `poglavlje` (
   `IDpoglavlje` int(11) NOT NULL AUTO_INCREMENT,
   `naziv` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
   `ukljuceno` tinyint(4) NOT NULL,
+  `timestamp` bigint(20) NOT NULL DEFAULT '0',
+  `obrisano` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`IDpoglavlje`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=53 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=54 ;
 
 --
 -- Izbacivanje podataka za tablicu `poglavlje`
 --
 
-INSERT INTO `poglavlje` (`IDpoglavlje`, `naziv`, `ukljuceno`) VALUES
-(1, 'Obrada teksta', 1),
-(3, 'Access', 1),
-(2, 'Excel', 1);
+INSERT INTO `poglavlje` (`IDpoglavlje`, `naziv`, `ukljuceno`, `timestamp`, `obrisano`) VALUES
+(1, 'Obrada teksta', 1, 0, 0),
+(3, 'Access', 1, 0, 0),
+(2, 'Excel', 1, 0, 0),
+(4, 'PowerPoint', 1, 1450643333, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
